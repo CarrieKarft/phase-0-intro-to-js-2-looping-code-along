abf7a9e888f7409cbd90542aab2d9a9946ae73d1
Carrie Kraft <carriekarft@gmail.com>
2022-11-29 18:02:23 -0800

    commit tree bb0ab750f5228357a7ba01bf8db59a8e6b15a029

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for(let gift of gifts) {
+        console.log(`Wrapped ${gift} and added a bow!`);
+    }
+}
+wrapGifts(gifts);
+
+function writeCards(names, event) {
+    const cardMessages = []
+    for(let i = 0; i < names.length; i++) {
+        cardMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    };
+    return cardMessages;
+}
+
+function countDown() {
+    let i = 10;
+    while(i > -1) {
+        console.log(i)
+        i--;
+    }
+}
+
+countDown()
